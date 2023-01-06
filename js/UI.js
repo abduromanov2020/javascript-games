f481c22766b35c844bea700e2f4fe5c9cf4253b3
abduromanov2020 <abduromanov2020@gmail.com>
2023-01-06 20:30:41 +0700

    initial commit

---
+++ js/UI.js
@@ -0,0 +1,97 @@
+export class UI {
+  constructor(game) {
+    this.game = game;
+    this.fontSize = 30;
+    this.fontFamily = "Creepster";
+    this.livesImage = document.getElementById("lives");
+  }
+
+  draw(context) {
+    context.save();
+    context.shadowOffsetX = 1;
+    context.shadowOffsetY = 1;
+    context.shadowBlur = 7;
+    context.shadowColor = "white";
+    context.font = this.fontSize + "px " + this.fontFamily;
+    context.textAlign = "left";
+    context.fillStyle = this.game.fontColor;
+
+    // Score
+    context.fillText("Score " + this.game.score, 20, 50);
+
+    // timer
+    context.font = this.fontSize * 0.8 + "px " + this.fontFamily;
+    context.fillText("Time " + (this.game.time * 0.001).toFixed(1), 20, 80);
+
+    // lives
+    for (let i = 0; i < this.game.lives; i++) {
+      context.drawImage(this.livesImage, 30 * i + 20, 95, 25, 25);
+    }
+
+    // level
+    if (this.game.score % 10000 < 1500 && this.game.score !== 0) {
+      context.textAlign = "center";
+      context.font = this.fontSize * 2 + "px " + this.fontFamily;
+      this.game.level = Math.floor(this.game.score / 10000 + 1);
+      context.fillText(
+        "Level " + this.game.level,
+        this.game.width * 0.5,
+        this.game.height * 0.5 - 180
+      );
+    }
+
+    // game over
+    if (this.game.gameOver) {
+      context.textAlign = "center";
+      context.font = this.fontSize * 3 + "px " + this.fontFamily;
+      if (this.game.lives !== 0) {
+        context.fillText(
+          "Winner Chiken Dinner",
+          this.game.width * 0.5,
+          this.game.height * 0.5 - 20
+        );
+      } else {
+        context.fillText(
+          "Game Over",
+          this.game.width * 0.5,
+          this.game.height * 0.5 - 20
+        );
+        context.font = this.fontSize * 1 + "px " + this.fontFamily;
+        context.fillText(
+          'Press "R" to restart',
+          this.game.width * 0.5,
+          this.game.height * 0.5 + 20
+        );
+      }
+    }
+
+    // Cover
+    if (
+      this.game.player.currentState === this.game.player.states[0] &&
+      this.game.player.x == 0
+    ) {
+      context.textAlign = "center";
+      context.fillStyle = "#111";
+      context.shadowColor = "white";
+      context.shadowOffsetX = 1;
+      context.shadowOffsetY = 1;
+      context.shadowBlur = 7;
+      context.font = this.fontSize * 3.5 + "px " + this.fontFamily;
+      context.fillText(
+        "Momob Adventure",
+        this.game.width * 0.5,
+        this.game.height * 0.5 - 150
+      );
+
+      context.font = this.fontSize * 1 + "px " + this.fontFamily;
+      context.fillText(
+        `Press 🡸 or 🡺  to play 
+      Press   "SHIFT"  to use power up 
+      Press 🡻 to pause`,
+        this.game.width * 0.5,
+        this.game.height * 0.5 + 220
+      );
+    }
+    context.restore();
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,156 @@
+import { Player } from "./player.js";
+import { InputHandler } from "./input.js";
+import { Background } from "./background.js";
+import {
+  FlyingEnemy,
+  GroundEnemy,
+  ClimbingEnemy,
+  EnemyBat,
+  EnemyGhost1,
+} from "./enemies.js";
+import { UI } from "./ui.js";
+
+window.addEventListener("load", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 1500;
+  canvas.height = 500;
+
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.groundMargin = 83;
+      this.speed = 0;
+      this.maxSpeed = 6;
+      this.background = new Background(this);
+      this.player = new Player(this);
+      this.input = new InputHandler(this);
+      this.UI = new UI(this);
+      this.enemies = [];
+      this.particles = [];
+      this.collisions = [];
+      this.floatingMessages = [];
+      this.maxParticles = 50;
+      this.enemyTimer = 0;
+      this.enemyInterval = 1000;
+      this.debug = false;
+      this.score = 0;
+      this.fontColor = "black";
+      this.time = 0;
+      this.maxTime = 1000;
+      this.gameOver = false;
+      this.lives = 5;
+      this.level = 1;
+      this.player.currentState = this.player.states[0];
+      this.player.currentState.enter();
+    }
+
+    update(deltaTime) {
+      this.background.update();
+      this.player.update(this.input.keys, deltaTime);
+
+      if (this.player.currentState !== this.player.states[0]) {
+        this.time += deltaTime;
+
+        this.enemies.forEach((enemy) => {
+          enemy.update(deltaTime);
+        });
+
+        // handleEnemies
+        if (this.enemyTimer > this.enemyInterval) {
+          this.enemyTimer = 0;
+          this.addEnemy();
+        } else this.enemyTimer += deltaTime;
+      }
+
+      // handleParticles
+      this.particles.forEach((particle, index) => {
+        particle.update();
+      });
+      if (this.particles.length > this.maxParticles)
+        this.particles.length = this.maxParticles;
+
+      // handleFloatingMessages
+      this.floatingMessages.forEach((message) => {
+        message.update(deltaTime);
+      });
+
+      // handleCollisions
+      this.collisions.forEach((collision, index) => {
+        collision.update(deltaTime);
+      });
+
+      this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion);
+      this.particles = this.particles.filter(
+        (particle) => !particle.markedForDeletion
+      );
+      this.collisions = this.collisions.filter(
+        (collision) => !collision.markedForDeletion
+      );
+      this.floatingMessages = this.floatingMessages.filter(
+        (message) => !message.markedForDeletion
+      );
+    }
+
+    draw(context) {
+      this.background.draw(context);
+      this.player.draw(context);
+
+      this.enemies.forEach((enemy) => {
+        enemy.draw(context);
+      });
+
+      this.particles.forEach((particle) => {
+        particle.draw(context);
+      });
+
+      this.collisions.forEach((collision) => {
+        collision.draw(context);
+      });
+
+      this.floatingMessages.forEach((message) => {
+        message.draw(context);
+      });
+
+      this.UI.draw(context);
+    }
+
+    addEnemy() {
+      if (this.speed > 0 && Math.random() < 0.5)
+        this.enemies.push(new GroundEnemy(this));
+      else if (this.speed > 0 && this.level == 2)
+        this.enemies.push(new ClimbingEnemy(this));
+      this.enemies.push(new FlyingEnemy(this));
+      this.enemies.push(new EnemyBat(this));
+      this.enemies.push(new EnemyGhost1(this));
+    }
+
+    reset() {
+      if (
+        this.gameOver &&
+        window.addEventListener("keydown", (e) => {
+          if (e.key === "r") {
+            location.reload();
+          }
+        })
+      );
+    }
+  }
+
+  const game = new Game(canvas.width, canvas.height);
+  console.log(game);
+  let lastTime = 0;
+
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.update(deltaTime);
+    game.draw(ctx);
+    game.reset();
+    if (!game.gameOver) requestAnimationFrame(animate);
+  }
+
+  animate(0);
+});
